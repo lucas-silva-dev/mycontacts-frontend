f97c1385f8af4ce9caf4af51b451c880a0ff94c1
lucas <lucassilva.work@outlook.com>
2023-10-19 00:42:48 -0300

    add timer to toast and clean form after submit

--- src/components/Toast/ToastContainer/index.jsx
+++ src/components/Toast/ToastContainer/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import ToastMessage from '../ToastMessage';
 
 import { Container } from './styles';
@@ -9,10 +9,12 @@ export default function ToastContainer() {
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    function handleAddToast({ type, text }) {
+    function handleAddToast({ type, text, duration }) {
       setMessages((prevState) => [
         ...prevState,
-        { id: Math.random(), type, text },
+        {
+          id: Math.random(), type, text, duration,
+        },
       ]);
     }
 
@@ -21,14 +23,18 @@ export default function ToastContainer() {
     return () => toastEventManager.removeListener('addtoast', handleAddToast);
   }, []);
 
+  const handleRemoveMessage = useCallback((id) => {
+    setMessages((prevState) => prevState.filter((message) => message.id !== id));
+  }, []);
+
   return (
     <Container>
       {
         messages.map((message) => (
           <ToastMessage
             key={message.id}
-            type={message.type}
-            text={message.text}
+            message={message}
+            onRemoveMessage={() => handleRemoveMessage(message.id)}
           />
         ))
       }
--- src/components/Toast/ToastMessage/index.jsx
+++ src/components/Toast/ToastMessage/index.jsx
@@ -1,24 +1,42 @@
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Container } from './styles';
 
 import xCircleIcon from '../../../assets/images/icons/x-circle.svg';
 import checkCircleIcon from '../../../assets/images/icons/check-circle.svg';
 
-export default function ToastMessage({ text, type }) {
+export default function ToastMessage({
+  message, onRemoveMessage,
+}) {
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      onRemoveMessage(message.id);
+    }, message.duration || 5000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [message, onRemoveMessage]);
+
+  function handleRemoveToast() {
+    onRemoveMessage(message.id);
+  }
+
   return (
-    <Container type={type}>
-      {type === 'danger' && <img src={xCircleIcon} alt="x" />}
-      {type === 'success' && <img src={checkCircleIcon} alt="check" />}
-      <strong>{text}</strong>
+    <Container type={message.type} onClick={() => handleRemoveToast()} tabIndex={0} role="button">
+      {message.type === 'danger' && <img src={xCircleIcon} alt="x" />}
+      {message.type === 'success' && <img src={checkCircleIcon} alt="check" />}
+      <strong>{message.text}</strong>
     </Container>
   );
 }
 
 ToastMessage.propTypes = {
-  text: PropTypes.string.isRequired,
-  type: PropTypes.oneOf(['default', 'success', 'danger']),
-};
-
-ToastMessage.defaultProps = {
-  type: 'default',
+  message: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+    type: PropTypes.oneOf(['default', 'success', 'danger']).isRequired,
+    duration: PropTypes.number,
+  }).isRequired,
+  onRemoveMessage: PropTypes.func.isRequired,
 };
